75a6fb8cdae2d97521f3421ec9cedaa5ed4d56d2
HamdiAlaa* <alaahamdi57@gmail.com>
2019-10-26 11:38:16 +0100

    public key update

--- cluster.ts
+++ cluster.ts
@@ -3,8 +3,9 @@
 import * as azure from "@pulumi/azure";
 import * as azuread from "@pulumi/azuread";
 import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
 import * as config from "./config";
-const __config = require('../config/aks_service_infos.json');
+let __config = require('../config/aks_service_infos.json');
 
 // Create the AD service principal for the K8s cluster.
 const adApp = new azuread.Application("aks");
@@ -17,20 +18,20 @@ const adSpPassword = new azuread.ServicePrincipalPassword("aksSpPassword", {
 
 // Now allocate an AKS cluster.
 
-export const k8sCluster = new azure.containerservice.KubernetesCluster(`${__config.aks_config.cluster_name}`, {
+export const k8sCluster = new azure.containerservice.KubernetesCluster(`${__config.cluster_name}`, {
     resourceGroupName: config.resourceGroup.name,
-    location: config.location,
+    location: __config.location,
 
     agentPoolProfiles: [{
         name: "aksagentpool",
-        count: __config.aks_config.node_number,
-        vmSize: __config.aks_config.node_size,
+        count: __config.node_number,
+        vmSize: __config.node_size,
     
 
     }],
     dnsPrefix: `test-kube-dns`,
     linuxProfile: {
-        adminUsername: __config.aks_config.admin__username,
+        adminUsername: __config.admin__username,
         sshKey: {
             keyData: config.sshPublicKey,
         },
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 import * as helm from "@pulumi/kubernetes/helm";
-import { k8sProvider } from "./cluster";
+import { k8sCluster, k8sProvider } from "./cluster";
 
 let __config = require('../config/aks_service_infos.json');
 
@@ -13,12 +13,13 @@ const service = new helm.v2.Chart(
     {
         repo: __config.services[index].repo,
         chart: __config.services[index].chart,
-        version: __config.services[index].version,
+        version: __config.version,
     },
     { providers: { kubernetes: k8sProvider } },
 );
     }}
 // export let cluster = k8sCluster.name;
+export let kubeConfig = k8sCluster.kubeConfigRaw;
 export let provider = k8sProvider;
 // export let serviceIP = apache
 //     .getResourceProperty("v1/Service", "apache-apache","status")
