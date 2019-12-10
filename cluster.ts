d4bc2e9fb2cf0208131b1381b74602e5e3838004
HamdiAlaa* <alaahamdi57@gmail.com>
2019-12-10 12:36:00 +0100

    multi stack project

--- cluster.ts
+++ cluster.ts
@@ -1,44 +1,34 @@
-// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
 import * as azure from "@pulumi/azure";
-import * as azuread from "@pulumi/azuread";
 import * as k8s from "@pulumi/kubernetes";
 import * as config from "./config";
-let __config = require('../config/aks_service_infos.json');
+let __ = require('../config/__aks.json');
 
-// Create the AD service principal for the K8s cluster.
-const adApp = new azuread.Application("aks");
-const adSp = new azuread.ServicePrincipal("aksSp", { applicationId: adApp.applicationId });
-const adSpPassword = new azuread.ServicePrincipalPassword("aksSpPassword", {
-    servicePrincipalId: adSp.id,
-    value: config.password,
-    endDate: "2099-01-01T00:00:00Z",
-});
+
 
 // Now allocate an AKS cluster.
 
-export const k8sCluster = new azure.containerservice.KubernetesCluster(`${__config.aks_config.cluster_name}`, {
+export const k8sCluster = new azure.containerservice.KubernetesCluster(`${__.aks_config.cluster_name}`, {
     resourceGroupName: config.resourceGroup.name,
-    location: __config.aks_config.location,
+    location: __.aks_config.location,
 
     agentPoolProfiles: [{
         name: "aksagentpool",
-        count: __config.aks_config.node_number,
-        vmSize: __config.aks_config.node_size,
+        count: __.aks_config.node_number,
+        vmSize: __.aks_config.node_size,
     
 
     }],
     dnsPrefix: `test-kube-dns`,
     linuxProfile: {
-        adminUsername: __config.aks_config.admin__username,
+        adminUsername: __.aks_config.admin__username,
         sshKey: {
             keyData: config.sshPublicKey,
         },
     },
 
     servicePrincipal: {
-        clientId: adApp.applicationId,
-        clientSecret: adSpPassword.value,
+        clientId: config.adApp.applicationId,
+        clientSecret: config.adSpPassword.value,
     },
 });
 
--- config.ts
+++ config.ts
@@ -2,6 +2,8 @@
 
 import * as azure from "@pulumi/azure";
 import * as pulumi from "@pulumi/pulumi";
+import * as azuread from "@pulumi/azuread";
+
 let __config = require('../config/aks_service_infos.json');
 
 
@@ -12,3 +14,12 @@ const location = __config.aks_config.location
 export const sshPublicKey = config.require("sshPublicKey");
 export const resourceGroup = new azure.core.ResourceGroup("aks", { location });
 // 8GO?HVlP0witz-8+1aitzy]*caY1jz+[
+// Create the AD service principal for the K8s cluster.
+export const adApp = new azuread.Application("aks");
+
+const adSp = new azuread.ServicePrincipal("aksSp", { applicationId: adApp.applicationId });
+export const adSpPassword = new azuread.ServicePrincipalPassword("aksSpPassword", {
+    servicePrincipalId: adSp.id,
+    value: password,
+    endDate: "2099-01-01T00:00:00Z",
+});
